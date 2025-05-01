af480fb20e85e23a9b99d9cce09506352328453d
vinicius <dev.vbortus@gmail.com>
2025-05-01 15:26:50 -0300

    att o arquivo .js

--- desafioboss.js
+++ desafioboss.js
@@ -1,5 +1,5 @@
 const nomeHeroi = "Javaman";
-let xpHeroi = 10000;
+let xpHeroi = 0; //Colocar o XP do heroi
 let nivelHeroi = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
 
 if (xpHeroi <= 1000) {
